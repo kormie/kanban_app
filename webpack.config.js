f18a168a4c4ac1d1e377fb5cf39b97f15badc2c9
David <agile_check_in@Davids-MacBook-Pro.local>
2016-02-06 16:29:08 -0600

    [#00] linting and crap

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,58 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS ={
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   entry: PATHS.app,
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['eslint'],
+        include: PATHS.app
+      }
+    ],
+    loaders: [
+    {
+      test: /\.css$/,
+      loaders: ['style', 'css'],
+      include: PATHS.app
+    }
+   ]
   }
 };
+
+// Default configuration
+
+if (TARGET == 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if (TARGET == 'build') {
+  module.exports = merge(common, {});
+}
