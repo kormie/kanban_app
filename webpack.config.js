e2d6e46d2fc93a792334560484d8ae182a246028
David <agile_check_in@Davids-MacBook-Pro.local>
2016-02-06 16:59:51 -0600

    [#00] Setup complete

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ const PATHS ={
 
 const common = {
   entry: PATHS.app,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
@@ -26,6 +29,11 @@ const common = {
     {
       test: /\.css$/,
       loaders: ['style', 'css'],
+      include: PATHS.app
+    },
+    {
+      test: /\.jsx?$/,
+      loaders: ['babel?cacheDirectory'],
       include: PATHS.app
     }
    ]
