a9422bb248788c029ef00f1e400b55329f17e146
David <agile_check_in@Davids-MacBook-Pro.local>
2016-02-07 23:45:09 -0600

    [#00] Basics Complete

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,65 +1,45 @@
 import React from 'react';
+import {DragSource, DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
 
-export default class Note extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      editing: false
+const noteSource = {
+  beginDrag(props) {
+    return {
+      id: props.id,
     };
+  },
+  isDragging(props, monitor) {
+    return props.id === monitor.getItem().id;
   }
+}
+
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const targetId = targetProps.id;
+    const sourceProps = monitor.getItem();
+    const sourceId = sourceProps.id;
+
+    if(sourceId !== targetId) {
+      targetProps.onMove({sourceId, targetId});
+    }
+  }
+}
+
+@DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging()
+}))
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+  connectDropTarget: connect.dropTarget()
+}))
+export default class Note extends React.Component {
   render() {
-    if (this.state.editing) {
-      return this.renderEdit();
-    }
-
-    return this.renderNote();
+    const {connectDragSource, connectDropTarget, isDragging,
+           id, onMove, ...props} = this.props;
+    return connectDragSource(connectDropTarget(
+        <li style={{
+          opacity: isDragging ? 0 : 1
+        }}{ ...this.props}>{this.props.children}</li>
+    ));
   }
-
-  renderEdit = () => {
-    return <input type="text"
-      ref={
-        (e) => e ? e.selectionStart = this.props.task.length : null
-      }
-      autoFocus={true}
-      defaultValue={this.props.task}
-      onBlur={this.finishEdit}
-      onKeyPress={this.checkEnter} />;
-  };
-  renderNote = () => {
-    const onDelete = this.props.onDelete;
-
-    return (
-      <div onClick={this.edit}>
-        <span className="task">{this.props.task}</span>
-        {onDelete ? this.renderDelete() : null}
-      </div>
-    );
-  };
-  renderDelete = () => {
-    return <button
-      className="delete-note"
-      onClick={this.props.onDelete}>x</button>;
-  };
-  edit = () => {
-    this.setState({
-      editing: true
-    });
-  };
-  checkEnter = (e) => {
-    if (e.key == 'Enter') {
-      this.finishEdit(e);
-    }
-  };
-  finishEdit = (e) => {
-    const value = e.target.value;
-
-    if (this.props.onEdit && value.trim()) {
-      this.props.onEdit(value);
-
-      this.setState({
-        editing: false
-      });
-    }
-  };
 }
--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -1,6 +1,7 @@
 import uuid from 'node-uuid';
 import alt from '../libs/alt';
 import LaneActions from '../actions/LaneActions';
+import update from 'react-addons-update';
 
 class LaneStore {
   constructor() {
@@ -44,7 +45,10 @@ class LaneStore {
   }
   attachToLane({laneId, noteId}) {
     const lanes = this.lanes.map(lane => {
-      if (lane.id == laneId) {
+      if (lane.notes.indexOf(noteId) >= 0) {
+        lane.notes = lane.notes.filter(note => note !== noteId);
+      }
+      if (lane.id === laneId) {
         if (lane.notes.indexOf(noteId) === -1) {
           lane.notes.push(noteId);
         }
@@ -63,6 +67,32 @@ class LaneStore {
       }
       return lane;
     });
+    this.setState({lanes});
+  }
+  move({sourceId, targetId}) {
+    const lanes = this.lanes;
+    const sourceLane = lanes.filter(lane => {
+      return lane.notes.indexOf(sourceId) >= 0;
+    })[0];
+    const targetLane = lanes.filter(lane => {
+      return lane.notes.indexOf(targetId) >= 0;
+    })[0];
+    const sourceNoteIndex = sourceLane.notes.indexOf(sourceId);
+    const targetNoteIndex = targetLane.notes.indexOf(targetId);
+
+    if (sourceLane === targetLane) {
+      sourceLane.notes = update(sourceLane.notes, {
+        $splice: [
+          [sourceNoteIndex, 1],
+          [targetNoteIndex, 0, sourceId]
+        ]
+      });
+    }
+    else {
+      sourceLane.notes.splice(sourceNoteIndex, 1);
+      targetLane.notes.splice(targetNoteIndex, 0, sourceId);
+    }
+
     this.setState({lanes});
   }
 }
